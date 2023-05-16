71502b32360e24842495919ea43d49bbfc11f4e2
cyh11303 <cyh11303@gmail.com>
2023-05-16 23:13:47 +0900

    20230516

--- main/case_weather.php
+++ main/case_weather.php
@@ -1,76 +1,79 @@
 <?php
-switch($a){
+switch($_POST['weather']){
     case "서울":
     case "서울시":
     case "서울특별시":
-        $a=1;
+        $_POST['weather']="서울특별시";
         break;
     case "인천":
     case "인천시":
-    case "인천특별시":
-        $a=2;
+    case "인천광역시":
+        $_POST['weather']="인천광역시";
         break;
     case "대전":
     case "대전시":
     case "대전광역시":    
-        $a=3;
+        $_POST['weather']="대전광역시";
         break;
     case "대구":
     case "대구시":
     case "대구광역시":
-        $a=4;
+        $_POST['weather']="대구광역시";
         break;
     case "광주":
     case "광주시":
     case "광주광역시":
-        $a=5;
+        $_POST['weather']="광주광역시";
         break;
     case "부산":
     case "부산시":
     case "부산광역시":
-        $a=6;
+        $_POST['weather']="부산광역시";
         break;
     case "울산":
     case "울산시":
     case "울산광역시":
-        $a=7;
+        $_POST['weather']="울산광역시";
         break;
     case "세종":
     case "세종특별시":  
-        $a=8;
+        $_POST['weather']="세종특별시";
         break;
     case "경기도":
-        $a=31;
+    case "경기":
+        $_POST['weather']="경기도";
         break;
     case "강원도":
-        $a=32;
+    case "강원":
+        $_POST['weather']="강원도";
         break;    
     case "충북":
     case "충청북도":
-        $a=33;
+        $_POST['weather']="충청북도";
         break;
     case "충남":
     case "충청남도":    
-        $a=34;
+        $_POST['weather']="충청남도";
         break;
     case "경북":
     case "경상북도":
-        $a=35;
+        $_POST['weather']="경상북도";
         break;
     case "경남":
     case "경상남도":
-        $a=36;
+        $_POST['weather']="경상남도";
         break;
     case "전북":
     case "전라북도":
-        $a=37;
+        $_POST['weather']="부산광역시";
         break;
     case "전남":
     case "전라남도":        
-        $a=38;
+        $_POST['weather']="전라남도";
         break;
     case "제주":
-        $a=39;
+    case "제주광역시":
+        $_POST['weather']="제주광역시";
         break;
     default:
         echo "<script>alert('지역명을 입력하여주세요!');</script>";
